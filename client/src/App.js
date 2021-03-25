fbe1b6c70cff666ac49d526de4b8dee633dab859
Juliana <juliana.mohamedali@gmail.com>
2021-03-25 19:35:08 +0800

    proxy update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Hello, testing front end</h1>
-  );
+const axios = require('axios');
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       bookmarks: [],
+    };
+  };
+
+  fetchInfo = async() => {
+    try {
+      const response = await axios.get('/bookmark');
+      console.log(response);
+    } catch (err){
+      console.log(err.message);
+    }
+  };
+
+  componentDidMount = () => {
+    this.fetchInfo();
+  };
+  
+  render() {
+    return (
+      <div>
+        <h1> Hello, Welcome to my bookmark!</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
